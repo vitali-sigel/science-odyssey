8537b4d3709267144c72699cdfb3d9b6c0d11550
vitali-sigel <kontakt@vitali-sigel.de>
2024-03-21 11:18:19 +0100

    with three

--- src/effects/portalEffect.js
+++ src/effects/portalEffect.js
@@ -1,10 +1,10 @@
-// import * as THREE from "three";
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-// import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
-// import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
-// import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
-// import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
-// import { gsap } from "gsap";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
+import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
+import { gsap } from "gsap";
 
 export default class Portal {
     constructor(container) {
