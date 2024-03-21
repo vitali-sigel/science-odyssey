aa8e341766893926eb58e1de18b75237eb3e10a4
vitali-sigel <kontakt@vitali-sigel.de>
2024-03-21 10:08:20 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import path from "path";
 import glsl from "vite-plugin-glsl";
 
 // vite.config.js
@@ -28,28 +27,6 @@ export default defineConfig({
           },
         },
         external: ['jquery'],
-      },
-    },
-    // build: {
-    //     minify: true,
-    //     manifest: true,
-    //     rollupOptions: {
-    //         input: "./src/main.js",
-    //         output: {
-    //             // Remove 'format: "umd"' or set it to 'es' if you need ESM output
-    //             entryFileNames: "main.js",
-    //             compact: true,
-    //             globals: {
-    //                 jquery: "$",
-    //             },
-    //         },
-    //         external: ["jquery"],
-    //     },
-    // },
-    // resolve: {
-    //   alias: {
-    //     'three': path.resolve(__dirname, './node_modules/three'),
-    //     // Add other aliases if necessary
-    //   },
-    // },
+      }
+    }
 });
